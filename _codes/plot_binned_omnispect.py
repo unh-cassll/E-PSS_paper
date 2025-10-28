Updated directional spectra computation/plotting utilities to remove erroneous angular offset; also added difference in mean wave direction between the short waves of E-PSS/MEM and the direct approach.

_codes/plot_binned_omnispect.py
@@ -112,7 +112,7 @@ F_f_slope_disp_binned = np.reshape(k_rad_m_slope_disp,(1,len(k_rad_m_slope_disp)
 B_f_slope_disp_binned = np.reshape(f_Hz_slope,(1,len(k_rad_m_slope_disp)))*S_f_binned
 B_f_binned = np.reshape(k_rad_m_disp,(1,len(k_rad_m_disp)))**2*np.reshape(f_Hz,(1,len(f_Hz)))*F_f_binned
 
-cmap = plt.get_cmap('magma')
+cmap = plt.get_cmap('cividis')
 color_min = np.min(U_centers)
 color_max = np.max(U_centers)    
 colors = [cmap(j) for j in np.linspace(0,1, len(U_centers))]  # Get colors from the colormap

_codes/plot_comparison_of_directional_information.py
@@ -135,6 +135,15 @@ for run_ind in np.arange(0,num_runs):
     
     F_EPSS = ds_EPSS_spect['F_f_d'][:,:,run_ind]
     
+    d_EPSS = F_EPSS['direction'].data
+    
+    f_inds = F_EPSS['frequency'] > 0.35
+    Fd_EPSS = np.sum(F_EPSS[f_inds,:].data,axis=0)
+    
+    ind = np.argmax(Fd_EPSS)
+    
+    MWD_EPSS_short[run_ind] = d_EPSS[ind]
+    
     Ff_EPSS[run_ind,:] = F_EPSS.integrate('direction')
     
     inds_exclude = (F_ADCP["frequency"].data > f_cut_high) | (F_ADCP["frequency"].data < f_low_filt)
@@ -195,8 +204,13 @@ MWD_EPSS[inds_northerly] = MWD_EPSS[inds_northerly] + 180
 inds_northerly = MWD_EPSS > 90
 MWD_EPSS[inds_northerly] = MWD_EPSS[inds_northerly] - 180
 MWD_diff = MWD_EPSS-MWD_ADCP_shifted
-# MWD_diff[MWD_diff>90] = MWD_diff[MWD_diff>90] - 180
-# MWD_diff[MWD_diff<-90] = MWD_diff[MWD_diff<-90] + 180
+
+# Unwrap angular differences arising from 180 degree ambiguity (short waves)
+inds_northerly = MWD_EPSS_short < -90
+MWD_EPSS_short[inds_northerly] = MWD_EPSS_short[inds_northerly] + 180
+inds_northerly = MWD_EPSS_short > 90
+MWD_EPSS_short[inds_northerly] = MWD_EPSS_short[inds_northerly] - 180
+MWD_diff_short = MWD_EPSS_short-MWD_ADCP_shifted
 
 # %%
 
@@ -213,9 +227,21 @@ bin_95CI = 1.96*bin_std/bin_counts
 bin_upper = bin_means + bin_95CI
 bin_lower = bin_means - bin_95CI
 
+inds_short = ~np.isnan(MWD_diff_short)
+
+bin_means_short, bin_edges_short, binnumber_short = stats.binned_statistic(U10_m_s[inds],MWD_diff_short[inds], statistic='mean', bins=U10_bin_edges)
+bin_std_short, _, _ = stats.binned_statistic(U10_m_s[inds],MWD_diff_short[inds], statistic='std', bins=U10_bin_edges)
+bin_counts_short, _, _ = stats.binned_statistic(U10_m_s[inds], MWD_diff_short[inds], statistic='count', bins=U10_bin_edges)
+
+bin_95CI_short = 1.96*bin_std_short/bin_counts_short
+bin_upper_short = bin_means_short + bin_95CI_short
+bin_lower_short = bin_means_short - bin_95CI_short
+
 fig = plt.figure(figsize=(6,6))
 plt.fill_between(U10_bin_centers, bin_upper, bin_lower, color=color_list[2], alpha=0.25)
 plt.plot(U10_bin_centers,bin_means,'-',color=color_list[2],linewidth=1,label=r'$\theta_{E-PSS}-\theta_{ADCP}$')
+plt.fill_between(U10_bin_centers, bin_upper_short, bin_lower_short, color=color_list[4], alpha=0.25)
+plt.plot(U10_bin_centers,bin_means_short,'-',color=color_list[4],linewidth=1,label=r'$\theta_{E-PSS,short}-\theta_{direct}$')
 plt.plot([0,16],[0,0],'--',color='gray')
 plt.xlim(0,14)
 plt.yticks(np.arange(-360,360,30))
@@ -244,7 +270,7 @@ SPREAD_peak[:,1] = SPREAD_EPSS_peak
 
 labels = ['ADCP','E-PSS']
 
-run_ind = 135
+run_ind = 164
 spread_ADCP = SPREAD_ADCP[run_ind,:]
 spread_EPSS = SPREAD_EPSS[run_ind,:]
 

_codes/plot_directional_spectra.py
@@ -32,6 +32,8 @@ fn = path+'ASIT2019_wave_spectra_stats_timeseries_empirical_gain.nc'
 ds = nc.Dataset(fn)
 
 ds_other = nc.Dataset(path+'ASIT2019_supporting_environmental_observations.nc')
+
+ds_EPSS_spect = xr.open_dataset(path+'ASIT2019_EPSS_directional_spectra.nc')
     
 elev_m = ds['elev_m'][:]
 slope_north = ds['slope_north'][:]
@@ -62,7 +64,7 @@ num_samples = np.size(elev_m,axis=1)
 nfft = num_samples/4
 nperseg = nfft/2
 
-run_ind = 135
+run_ind = 165
 
 lowcut_filter = 0.05
 highcut_filter = 1
@@ -113,9 +115,7 @@ dataset_Pyxis_frequency = xr.Dataset(
 
 Ffd = dataset_Pyxis_frequency.Ffd
 
-F_EPSS = compute_dirspec_EPSS(elev_m[run_ind,:],slope_east[run_ind,:],slope_north[run_ind,:],fs_Hz,lowcut_filter,highcut_filter,nfft,nperseg,smoothnum)
-
-F_EPSS = trim_EPSS_dirspec(F_EPSS,Ffd,theta_halfwidth,f_cut_high)
+F_EPSS = ds_EPSS_spect['F_f_d'][:,:,run_ind]
 
 Ff_ADCP = dataset_ADCP.F_ADCP.integrate('direction')
 F_ADCP = dataset_ADCP.F_ADCP
@@ -197,7 +197,7 @@ ax0.set_ylim(1e-2,2e1)
 ax0.set_xlabel(r'$\theta$ [$\circ$]')
 ax0.set_ylabel('f [Hz]')
 ax0.text(0.04,0.93,'(a)',color='white',fontsize=12,ha='center',va='center',transform=ax0.transAxes)
-ax0.text(-81, 2.2e-2, 'MEM, ADCP',
+ax0.text(-37, 2.2e-2, 'MEM, ADCP',
          fontsize=12,
          color='black',
          bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5', alpha=1))
@@ -212,7 +212,7 @@ ax1.set_ylim(1e-2,2e1)
 ax1.set_xlabel(r'$\theta$ [$\circ$]')
 ax1.set_yticklabels([])
 ax1.text(0.04,0.93,'(b)',color='white',fontsize=12,ha='center',va='center',transform=ax1.transAxes)
-ax1.text(-155, 2.2e-2, 'MEM, E-PSS',
+ax1.text(-40, 2.2e-2, 'MEM, E-PSS',
          fontsize=12,
          color='black',
          bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5', alpha=1))

_codes/subroutines/mem_distribution.py
@@ -41,7 +41,7 @@ import numpy as np
 def mem_distribution(moments, smoothing=32):
     """Implementation of the Maximum Entropy Method"""
 
-    dirs =  xr.Variable(dims=("direction"), data=np.arange(-180,180,5)-90)
+    dirs =  xr.Variable(dims=("direction"), data=np.arange(-180,180,5))
 
     c1 = moments["a1"] + 1j*moments["b1"]
     c2 = moments["a2"] + 1j*moments["b2"]