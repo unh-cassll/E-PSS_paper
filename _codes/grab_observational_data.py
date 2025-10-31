abcfd5f9c67655f72ccef377b219b12e7bd57aa2
Nathan Laxague <anotherwavefromnathan@protonmail.com>
2025-10-31 10:23:40 -0400

    Changed makefile structure to ensure external data are grabbed before processing begins

--- _codes/grab_observational_data.py
+++ _codes/grab_observational_data.py
@@ -14,7 +14,7 @@ URL = "https://zenodo.org/records/17388350/files/"
 filenames_to_grab = ["ASIT2019_supporting_environmental_observations.nc", "ASIT2019_wave_spectra_stats_timeseries_empirical_gain.nc", "ASIT2019_wave_spectra_stats_timeseries_lab_gain.nc", "ASIT2019_wave_spectra_stats_timeseries_no_gain.nc", "Elfouhaily_et_al_1997_cumulative_mss.nc", "Piermont2025_DoLP_AoI_observations.nc", "Run051_example_Stokes_parameters.nc"]
 suffix = "?download=1"
 
-local_dir = '../_data'
+local_dir = '_data'
 
 # loop over the list of files to grab; download each one if a local copy doesn't exist
 for short_fname in filenames_to_grab:
