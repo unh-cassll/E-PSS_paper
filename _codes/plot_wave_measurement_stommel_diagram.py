Corrected erroneous factor of 1/(2*pi) in wave radian frequency for Stommel diagram generation script.

_codes/plot_wave_measurement_stommel_diagram.py
@@ -15,7 +15,7 @@ g = 9.81
 sigma = 0.072
 rho = 1020
 k = np.logspace(-2,4, 1000)
-omega = np.sqrt(g*k+sigma/rho*k**3)/(2*np.pi)
+omega = np.sqrt(g*k+sigma/rho*k**3)
 
 x = 2*np.pi/k
 y = 2*np.pi/omega
@@ -30,8 +30,8 @@ colors = ['#4C2882', '#367588', '#A52A2A', '#C39953', '#2A52BE']
 methods =['polarimetry','stereo video']
 
 rectangles = [
-    (0.002, 0.033, 2.5, 0.75),  # polarimetry (as it has been done)
-    (0.1, 0.2, 100, 10),        # stereo (as it has been done)
+    (0.0025, 0.033, 2.5, 1.25),  # polarimetry (as it has been done)
+    (0.4, 0.5, 100, 8),        # stereo (as it has been done)
     (5, 10, 0.8, 0.3),
     (15, 50, 1.5, 0.5),
     (30, 100, 0.5, 2.0)
@@ -42,7 +42,7 @@ for (x_pos, y_pos, width, height), color, method in zip(rectangles,colors,method
     plt.gca().add_patch(rect)
     plt.text(x_pos+width*0.85,y_pos*1.15,method,color=color,horizontalalignment='right')
 
-plt.text(30,40,'linear dispersion',horizontalalignment='right')
+# plt.text(30,40,'gravity-capillary linear dispersion',horizontalalignment='right')
 
 plt.xlim(0.001, 100)
 plt.ylim(0.01, 100)
@@ -64,4 +64,4 @@ line_ax.text(3, 10, 'buoys, gauges', fontsize=12, rotation=90, color=colors[4], 
 line_ax.annotate('', xy=(1, 100), xytext=(1, 11), 
                  arrowprops=dict(arrowstyle='->', color=colors[4], lw=3))
 
-plt.savefig('../_figures/wave_measurement_stommel_diagram.pdf',bbox_inches='tight')
+# plt.savefig('../_figures/wave_measurement_stommel_diagram.pdf',bbox_inches='tight')